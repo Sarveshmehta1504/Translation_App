12a07b6662aef6caf003303d3e009f6e1db40493
Sarvesh Mehta <sarveshmehta1504@gmail.com>
2024-07-04 01:28:04 +0530

    feat: Implement basic theming
    This commit implements basic theming for the Android app by adding the following:
    
    - A `Colors` object in the shared
     module to define common colors.
    - A `Color` object in the Android module to map shared colors to Compose colors.
    - A `TranslatorTheme` composable function to provide Material theme with custom colors, typography, and shapes.
    - Font resources for SF Pro Text (regular, medium, bold).
    
    This sets up the foundation for a consistent and customizable look and feel across the app.

--- androidApp/src/main/java/com/example/translationapp/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/translationapp/android/MainActivity.kt
@@ -3,13 +3,102 @@ package com.example.translationapp.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Shapes
+import androidx.compose.material.Surface
+import androidx.compose.material.Typography
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.translationapp.android.core.theme.darkColors
+import com.example.translationapp.android.core.theme.lightColors
+import dagger.hilt.android.AndroidEntryPoint
 
+@Composable
+fun TranslatorTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        darkColors
+    } else {
+        lightColors
+    }
+    val SfProText = FontFamily(
+        Font(
+            resId = R.font.sf_pro_text_regular,
+            weight = FontWeight.Normal
+        ),
+        Font(
+            resId = R.font.sf_pro_text_medium,
+            weight = FontWeight.Medium
+        ),
+        Font(
+            resId = R.font.sf_pro_text_bold,
+            weight = FontWeight.Bold
+        ),
+    )
+    val typography = Typography(
+        h1 = TextStyle(
+            fontFamily = SfProText,
+            fontWeight = FontWeight.Bold,
+            fontSize = 30.sp
+        ),
+        h2 = TextStyle(
+            fontFamily = SfProText,
+            fontWeight = FontWeight.Bold,
+            fontSize = 24.sp
+        ),
+        h3 = TextStyle(
+            fontFamily = SfProText,
+            fontWeight = FontWeight.Medium,
+            fontSize = 18.sp
+        ),
+        body1 = TextStyle(
+            fontFamily = SfProText,
+            fontWeight = FontWeight.Normal,
+            fontSize = 14.sp
+        ),
+        body2 = TextStyle(
+            fontFamily = SfProText,
+            fontWeight = FontWeight.Normal,
+            fontSize = 12.sp
+        ),
+    )
+    val shapes = Shapes(
+        small = RoundedCornerShape(4.dp),
+        medium = RoundedCornerShape(4.dp),
+        large = RoundedCornerShape(0.dp)
+    )
+
+    androidx.compose.material.MaterialTheme(
+        colors = colors,
+        typography = typography,
+        shapes = shapes,
+        content = content
+    )
+}
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
+            TranslateTheme {
+                Surface (
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ){
 
+                }
             }
         }
     }
---
+++ shared/src/commonMain/kotlin/com/example/translationapp/core/presentation/Colors.kt
@@ -0,0 +1,10 @@
+package com.example.translationapp.core.presentation
+
+object Colors {
+    val LightBlue = 0xFFA8A5BB
+    val LightBlueGrey = 0xFFF4F6F4
+    val AccentViolet = 0xFF5643C9
+    val TextBlack = 0xFF111111
+    val DarkGrey = 0xFF282C31
+
+}
